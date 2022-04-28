01c28101e8f8aad23570df83e6ea0800bddbc83f
cocos56 <zj175@139.com>
2022-04-28 19:32:48 +0800

    创建表（在models.py文件中）

--- bms/app/models.py
+++ bms/app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Book(models.Model):
+    # id = models.IntegerField() # 自动创建
+    name = models.CharField(max_length=256)
+    author = models.CharField(max_length=128)
+    publisher = models.CharField(max_length=128)
+    price = models.FloatField()
+    number = models.IntegerField()
