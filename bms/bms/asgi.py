609c75ff04cc0ba8abcc9642a1e283c488b0a0f6
cocos56 <zj175@139.com>
2022-04-28 10:29:48 +0800

    调整项目架构

--- bms/bms/asgi.py
+++ bms/bms/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for qh project.
+ASGI config for bms project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qh.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bms.settings')
 
 application = get_asgi_application()
