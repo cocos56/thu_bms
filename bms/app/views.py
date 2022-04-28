8482abf48cf141c13cb6da18e899a745dc8dc19b
cocos56 <zj175@139.com>
2022-04-28 11:38:06 +0800

    更新视图

--- bms/app/views.py
+++ bms/app/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import render, HttpResponse
 
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('欢迎使用')
+    # return HttpResponse('欢迎使用')
+    return render(request, 'index.html')
